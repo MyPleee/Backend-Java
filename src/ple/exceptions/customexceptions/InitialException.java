280d19175973652834302e8cb0dd394fa043a155
heesom <heesom.hs@gmail.com>
2024-07-16 13:09:21 +0900

    refac: remove deprecated method in initialexception

--- src/ple/exceptions/customexceptions/InitialException.java
+++ src/ple/exceptions/customexceptions/InitialException.java
@@ -5,21 +5,6 @@ import ple.exceptions.exceptiontypes.InitialErrorType;
 public class InitialException extends PleException{
 
 	private static final long serialVersionUID = 8701619814869045856L;
-
-	@Deprecated
-	public InitialException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
-	
-	@Deprecated
-	public InitialException(Throwable cause) {
-		super(cause);
-	}
-	
-	@Deprecated
-	public InitialException(InitialErrorType initialErrorType, String msg) {
-		super(initialErrorType, msg);
-	}
 	
 	public InitialException(Throwable cause, InitialErrorType initialErrorType) {
 		super(cause, initialErrorType);
