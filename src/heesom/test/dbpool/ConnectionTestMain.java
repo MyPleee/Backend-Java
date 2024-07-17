c46de7fabd69df0a655b68b443ea64443cb69f11
heesom <heesom.hs@gmail.com>
2024-07-17 13:41:27 +0900

    test: revise comments on connectiontest

--- src/heesom/test/dbpool/ConnectionTestMain.java
+++ src/heesom/test/dbpool/ConnectionTestMain.java
@@ -9,22 +9,34 @@ public class ConnectionTestMain {
 		dbPoolManager.initializePool();
 		
 		StopWatch stopWatch = StopWatch.getInstance();
-		stopWatch.startStopWatch();
 		
-		// db 커넥션 필요할 시 맺을 때 elapse time:2619
+		/*
+		 * 1. db 커넥션 필요할 시 맺을 때 
+		 * ==================================
+		 * start time - 2024/07/17 13:38:36.319
+		 * elapse time - 3400
+		 * stop time - 2024/07/17 13:38:39.719
+		 * ==================================
+		 */
+		stopWatch.startStopWatch();
 		TestDaoWithDb testDaoWithDb = new TestDaoWithDb();
 		for (int i = 0; i < 1000; i++) {
 			testDaoWithDb.selectData(1);
 		}
 		
-		stopWatch.stopStopWatch();
 		stopWatch.print();
 		
-		stopWatch.resetStopWatch();
 		
-		
+		/*
+		 * 2. pool 사용할 경우
+		 * ==================================
+		 * start time - 2024/07/17 13:38:39.725
+		 * elapse time - 194
+		 * stop time - 2024/07/17 13:38:39.919
+		 * ==================================
+
+		 */
 		stopWatch.startStopWatch();
-		// pool 사용할 경우 elapse time:169
 		TestDaoWithDbPool testDaoWithDbPool = new TestDaoWithDbPool();
 		for (int i = 0; i < 1000; i++) {
 			testDaoWithDbPool.selectData(1);
