d277b2a0bec6896308c64501e2cf677204c33c94
heesom <heesom.hs@gmail.com>
2024-07-19 11:12:54 +0900

    feat: finished Logoutcontroller

--- src/ple/controllers/usercontrollers/LogoutController.java
+++ src/ple/controllers/usercontrollers/LogoutController.java
@@ -4,16 +4,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ple.controllers.controllerinterfaces.CommonController;
+import ple.controllers.usercontrollers.dto.UserDTO;
+import ple.controllers.usercontrollers.util.JSONParsing;
+import ple.controllers.usercontrollers.util.SessionManager;
 import ple.exceptions.customexceptions.PleException;
 import ple.exceptions.exceptionhandling.ExceptionHandler;
 
 public class LogoutController implements CommonController{
 
-	//TODO
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws PleException{
-
+		System.out.println("==> LogoutController, doGet");
+	    
+		SessionManager.invalidateSession(req, resp);
 		
+		System.out.println("<== LogoutController, doGet");
 	}
 
 	@Override
