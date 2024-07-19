1918c020f6e0928b14a3927279e5326b99fd9892
heesom <heesom.hs@gmail.com>
2024-07-19 09:20:24 +0900

    refac: add cookie feat in sessionManager

--- src/ple/controllers/usercontrollers/util/SessionManager.java
+++ src/ple/controllers/usercontrollers/util/SessionManager.java
@@ -1,12 +1,19 @@
 package ple.controllers.usercontrollers.util;
 
-import javax.servlet.SessionCookieConfig;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class SessionManager {
 	
-	public static void setValidUserSession(HttpServletRequest req, String userId) {
+	/**
+	 * 세션 생성 후 쿠키 추가
+	 * @param userId
+	 * @param req
+	 * @param resp
+	 */
+	public static void setValidUserSession(String userId, HttpServletRequest req, HttpServletResponse resp) {
 		
 		// 클라이언트가 세션 쿠키를 보내지 않았을 경우 null 반환, 세션 하이재킹 방지
         HttpSession session = req.getSession(false); 
@@ -14,26 +21,43 @@ public class SessionManager {
         if (session != null) {
             session.invalidate();
         }
-        session = req.getSession(true); // 새로운 세션 생성
+        HttpSession newSession = req.getSession(true); // 새로운 세션 생성
 
-        if (session != null) {
-            session.setMaxInactiveInterval(18000);
-            session.setAttribute("userId", userId);
+        if (newSession != null) {
+        	newSession.setMaxInactiveInterval(18000);
+        	newSession.setAttribute("userId", userId);
         } else {
             throw new IllegalStateException("Failed to create a new session.");
         }
+        
+        Cookie cookie = new Cookie("ple-session", newSession.getId());
+        cookie.setPath("/");
+        cookie.setMaxAge(18000); // 5시간 (초 단위)
+        cookie.setHttpOnly(true);
+        //cookie.setSecure(true);
+        
+        resp.addCookie(cookie);
 
     }
 
     /**
-     * 세션 무효화
+     * 세션 무효화하고 쿠키 삭제
      * @param req
      */
-    public static void invalidateSession(HttpServletRequest req) {
+    public static void invalidateSession(HttpServletRequest req, HttpServletResponse resp) {
         HttpSession session = req.getSession(false);
         if (session != null) {
             session.invalidate();
         }
+        
+        // 클라이언트 쪽 쿠키 삭제
+        Cookie cookie = new Cookie("ple-session", null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0); // 쿠키 만료 시간 0으로 설정
+        cookie.setHttpOnly(true);
+        //cookie.setSecure(true); // 필요한 경우 사용
+
+        resp.addCookie(cookie);
     }
     
     /**
@@ -42,7 +66,7 @@ public class SessionManager {
      * @param userId
      * @return
      */
-    public static boolean isValidSession(HttpServletRequest req, String userId) {
+    public static boolean isValidSession(String userId, HttpServletRequest req) {
         HttpSession session = req.getSession(false);
 
         if (session != null && !session.getAttribute("userId").equals(userId)) {
